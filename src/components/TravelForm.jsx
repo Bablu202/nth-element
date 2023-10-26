6ed300c8a0763ef19d44ec4c05874bbeb9a2ccce
aware <aware9758@gmail.com>
2023-10-26 10:33:53 +0200

    res-1

--- src/components/TravelForm.jsx
+++ src/components/TravelForm.jsx
@@ -8,40 +8,46 @@ export default function TravelForm({
   return (
     <div>
       <form className="travel__form" onSubmit={handleAddTravelItems}>
-        <h3 className="heading-3">Dont't forget what you need!</h3>
+        <h3 className="travel__form--heading heading-3">
+          Dont't forget what you need!
+        </h3>
         {/*SELECT FROM 1 - 20 ------------*/}
+        <div className="travel__from--inputs">
+          <select
+            className="select__box"
+            value={moreTravelElement.quantity}
+            onChange={(e) => {
+              setMoreTravelElement({
+                ...moreTravelElement,
+                quantity: e.target.value,
+              });
+            }}
+          >
+            {Array.from({ length: 20 }, (e, i) => i + 1).map((num) => (
+              <option className="select__option" key={num}>
+                {num}
+              </option>
+            ))}
+          </select>
+          {/*ADD ITEM HERE ------------*/}
+          <input
+            className="travel__form--input"
+            type="text"
+            placeholder="thing's to pack"
+            value={moreTravelElement.item}
+            onChange={(e) =>
+              setMoreTravelElement({
+                ...moreTravelElement,
+                item: e.target.value,
+              })
+            }
+          />
+          {/*SUBMIT BUTTON ------------*/}
 
-        <select
-          className="select__box"
-          value={moreTravelElement.quantity}
-          onChange={(e) => {
-            setMoreTravelElement({
-              ...moreTravelElement,
-              quantity: e.target.value,
-            });
-          }}
-        >
-          {Array.from({ length: 20 }, (e, i) => i + 1).map((num) => (
-            <option className="select__option" key={num}>
-              {num}
-            </option>
-          ))}
-        </select>
-        {/*ADD ITEM HERE ------------*/}
-        <input
-          className="travel__form--input"
-          type="text"
-          placeholder="thing's to pack"
-          value={moreTravelElement.item}
-          onChange={(e) =>
-            setMoreTravelElement({ ...moreTravelElement, item: e.target.value })
-          }
-        />
-        {/*SUBMIT BUTTON ------------*/}
-
-        <button className=" btn btn--add" onClick={handleAddTravelItems}>
-          Add
-        </button>
+          <button className=" btn btn--add" onClick={handleAddTravelItems}>
+            Add
+          </button>
+        </div>
       </form>
     </div>
   );
