211fac9065c717f7c8297431aecef1d8cd49a7f3
aware <aware9758@gmail.com>
2023-10-27 11:44:05 +0200

    authSupabase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import RatingE from "./components/smallElements/RatingE";
 import Travel from "./pages/Travel";
+import React, { useState, useEffect } from "react";
+import { SignUp, Login, Homepage } from "./authInOut/index";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState(false);
+
+  if (token) {
+    sessionStorage.setItem("token", JSON.stringify(token));
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
+      setToken(data);
+    }
+  }, []);
+
   return (
-    <>
-      <Travel />
-      {/* <RatingE ss="#ffb30d" maxrating={5} /> */}
-    </>
+    <div>
+      <Routes>
+        <Route path={"/signup"} element={<SignUp />} />
+        <Route path={"/"} element={<Login setToken={setToken} />} />
+        {token ? (
+          <Route path={"/homepage"} element={<Homepage token={token} />} />
+        ) : (
+          ""
+        )}
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
