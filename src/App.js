a432c7a2bf1d35393abf982606fb88473e1a719d
aware <aware9758@gmail.com>
2023-10-27 10:39:55 +0200

    rat

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Travel from "./pages/Travel";
 function App() {
   return (
     <>
-      {/* <Travel /> */}
-      <RatingE ss="#ffb30d" maxrating={5} />
+      <Travel />
+      {/* <RatingE ss="#ffb30d" maxrating={5} /> */}
     </>
   );
 }
